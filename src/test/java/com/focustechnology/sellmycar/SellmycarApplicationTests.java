5330887c32799b2b7d7d2ae5dffb378a44a24d29
Ahmed Aziz <ahmed83@me.com>
2023-03-26 12:20:37 +0200

    setup project

---
+++ src/test/java/com/focustechnology/sellmycar/SellmycarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.focustechnology.sellmycar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellmycarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
